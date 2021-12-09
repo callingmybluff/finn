28d841aaaef29ed031e0d1320e4aca3bc48c594a
callingmybluff <ismaeel.abuabdallah@gmail.com>
2021-12-09 18:03:36 +0100

    2nd round Footer

--- finn/src/components/footer/FooterElements.tsx
+++ finn/src/components/footer/FooterElements.tsx
@@ -2,10 +2,12 @@ import Styled from 'styled-components'
 
 export const Container = Styled.div`
   display: flex;
-  flex-flow: row;
   background-color: ${props => props.theme.color.neutral.veryDarkViolet};
 
+  padding: ${props => props.theme.size.margin};
+  flex-flow: row;
   @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    padding: ${props => props.theme.size.marginMobile};
     flex-flow: column;
   }
 `
@@ -17,11 +19,16 @@ export const Part = Styled.div`
 
 export const Title = Styled.h1`
   color: white;
-  text-align: center;
+  text-align: left;
+  @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    text-align: center;
+  }
 `
 
-export const PartTitle = Styled.h2`
+export const PartTitle = Styled.h4`
   color: white;
+  padding-bottom: 2ex;
+  margin: 0;
 
   text-align: left;
   @media screen and (max-width: ${props => props.theme.size.mobile}) {
@@ -31,6 +38,8 @@ export const PartTitle = Styled.h2`
 
 export const PartLink = Styled.div`
   color: ${props => props.theme.color.neutral.gray};
+  font-weight: ${props => props.theme.font.weight.light};
+  padding-bottom: 1ex;
 
   text-align: left;
   @media screen and (max-width: ${props => props.theme.size.mobile}) {
@@ -42,6 +51,10 @@ export const SocialMedia = Styled.div`
   display: flex;
   flex-flow: row;
   justify-content: space-around;
+
+  @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    margin 4ex;
+  }
 `
 
 export const SocialMediaIcon = Styled.img`
