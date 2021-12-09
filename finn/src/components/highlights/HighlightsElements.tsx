50dd7577bf7a4b44e98f4808c1fbb6316e6ae971
callingmybluff <ismaeel.abuabdallah@gmail.com>
2021-12-09 17:42:03 +0100

    Second round Highlights

--- finn/src/components/highlights/HighlightsElements.tsx
+++ finn/src/components/highlights/HighlightsElements.tsx
@@ -5,7 +5,9 @@ export const Container = Styled.div`
 
   flex-flow: row;
   background: linear-gradient(${props => props.theme.color.primary.cyan}, ${props => props.theme.color.primary.cyan}) no-repeat center/70% 10px;
+  margin: ${props => props.theme.size.margin};
   @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    margin: ${props => props.theme.size.marginMobile};
     flex-flow: column;
     background-size: 10px 70%;
   }
@@ -15,11 +17,14 @@ export const CardContainer = (margin: string) => Styled.div`
   display: flex;
   flex-flow: column;
   background-color: white;
-  margin-left: 1%;
-  margin-right: 1%;
+  margin: 1ex;
+  border-radius: .5ex;
+  padding: 1ex;
+  height: max-content;
 
   margin-top: ${margin};
   @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    margin: 0;
     margin-top: 20%;
     margin-bottom: 1%;
   }
@@ -44,10 +49,20 @@ export const LogoContainer = Styled.div`
 export const Logo = Styled.img`
 `
 
-export const Title = Styled.h1`
+export const Title = Styled.h3`
   flex: 0;
+
+  @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    text-align: center;
+  }
 `
 
 export const Body = Styled.p`
   flex: 0;
+  color: ${props => props.theme.color.neutral.gray};
+  font-weight: ${props => props.theme.font.weight.light};
+
+  @media screen and (max-width: ${props => props.theme.size.mobile}) {
+    text-align: center;
+  }
 `
